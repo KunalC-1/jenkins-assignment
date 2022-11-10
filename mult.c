8a8fb86db3ec981abd2e08718c2b6d853afba7aa
Kunal Chaudhari <chaudhariks19.comp@coep.ac.in>
2022-11-10 21:11:24 +0530

    add addition function

--- mult.c
+++ mult.c
@@ -3,9 +3,14 @@
 int multiply(int a, int b){
     return a * b;
 }
+
+int addition(int a, int b){
+    return a + b;
+}
 int main(){
     int a = 5;
     int b = 8;
     printf("Multiplication of Two Numbers : %d", multiply(a, b));
+    printf("\nAddition of Two Numbers : %d", addition(a, b));
     return 0;
 };
